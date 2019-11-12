65ee5f304a1222351b66302cab553f257f5656ae
Io Mintz <io@mintz.cc>
2019-11-12 20:21:54 +0000

    lines_to_statements: handle more than one statement properly

--- sql_remove_comma.py
+++ sql_remove_comma.py
@@ -16,15 +16,19 @@ def remove_trailing_comma(stmt):
 				return
 
 def lines_to_statements(it):
-	buf = []
+	import io
+	buf = io.StringIO()
 	for line in it:
-		buf.append(line)
-		stmts = sqlparse.parse(''.join(buf))
-		if len(stmts) > 1:
+		buf.write(line)
+		stmts = sqlparse.parse(buf.getvalue())
+		if len(stmts) > 1 and str(stmts[1]).strip():
 			yield stmts[0]
-			last_line = buf[-1]
-			buf.clear()
-			buf.append(last_line)
+			buf.seek(0); buf.truncate()
+			for trailing_stmt in sqlparse.split(line):
+				buf.write(str(trailing_stmt))
+				buf.write('\n')
+
+	yield from sqlparse.parse(buf.getvalue())
 
 def main():
 	import sys
